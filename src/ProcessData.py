a4b8f5329cf0914ddd9af018b58429365ff1384d
Layne <lhymel9@lsu.edu>
2017-08-31 14:39:37 -0500

    Added unique word processing

--- src/ProcessData.py
+++ src/ProcessData.py
@@ -18,28 +18,55 @@ def process_data_dict(dictionary):
     }
     return processed_dict
 
+def process_survey_dict(dictionary):
+    """ @param: A dictionary formed from job survey data (similar to DataProfile _dict member) """
+    """ @return: A processed (ready for comparison) job survey data dictionary """
+
+    processed_dict = {
+        "title": preprocess_str(dictionary["title"]),
+        "description": preprocess_str(dictionary["description"]),
+        "naics": dictionary["naics"]
+    }
+    return processed_dict
+
 
 def preprocess_str(single):
     """ @param: A string """
-    """ @return: A lowercased, stopped, split list of the string """
+    """ @return: A lowercased, stopped, split, unique list of the string """
 
     unlemmatized = [word for word in single.lower().split() if word.isalpha() and word not in stops]
-    return [wnl.lemmatize(word) for word in unlemmatized]
-
+    return uniquify([wnl.lemmatize(word) for word in unlemmatized])
 
 def preprocess_str_list(arr):
     """ @param: A list of strings """
-    """ @return: A lowercased and stopped list """
+    """ @return: A list of lists of lowercased and stopped words """
 
-    unlemmatized = [word.lower() for word in arr if word.isalpha() and word not in stops]
-    return [wnl.lemmatize(word) for word in unlemmatized]
-
+    return [preprocess_str(statement) for statement in arr]
 
 def preprocess_tup_list(arr):
     """ @param: A list of 2-variable string tuples """
-    """ @return: A lowercased and stopped list of 2-variable string tuples """
+    """ @return: A list of 2-variable list, lowercased, stopped, string tuples """
 
     titles, descriptions = zip(*arr) #unzip operation
     titles = preprocess_str_list(list(titles))
     descriptions = preprocess_str_list(list(descriptions))
     return list(zip(titles, descriptions))
+
+
+def uniquify(arr)
+    """ @param: A list of strings """
+    """ @return: The same list with duplicate elements removed """
+
+    if has_dup(arr):
+        return list(set(arr))
+    return arr
+
+def has_dup(arr):
+    """ @param: A list of strings """
+    """ @return: A boolean value corresponding to whether or not the input list has unique elements """
+
+    for i in range(0, len(arr)):
+        for j in range(i+1, len(arr)):
+            if arr[i] == arr[j]:
+                return True
+    return False
